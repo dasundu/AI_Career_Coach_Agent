4d7fabe10e71a6bed32728b60c469b4a3b18e187
Dasun Dushmantha <dasunathauda99@gmail.com>
2025-07-15 12:02:01 +0530

    Update route.tsx

--- app/api/ai-career-chat-agent/route.tsx
+++ app/api/ai-career-chat-agent/route.tsx
@@ -32,7 +32,7 @@ export async function POST(req :any) {
 
 }
 
-export async function getRuns(runId:string) {
+ async function getRuns(runId:string) {
     const result=await axios.get(process.env.INNGEST_SERVER_HOST+'/v1/events/' + runId + '/runs',{
         headers:{
             Authorization: `Bearer ${process.env.INNGEST_SIGNING_KEY}`
